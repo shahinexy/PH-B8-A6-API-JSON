69528b4a8bf6c4330f9f093a0fc879beaa600d6e
shahinexy <shahinexy@gmail.com>
2024-03-01 18:06:48 +0600

    work on sorting

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,34 +9,48 @@ const getBtn = (btns) => {
     const btnContiner = document.getElementById('btn_container');
     for (const btn of btns) {
         const button = document.createElement('button');
-        button.className = 'btn rounded-none text-lg font-semibold text-white bg-indigo-400 px-5 py-2';
+        button.className = 'btn categori_btn rounded-none text-lg font-semibold text-white bg-indigo-400 px-5 py-2';
         button.innerText = btn.category;
-        button.addEventListener('click', () => getBtnID(btn.category_id))
+        button.addEventListener('click', () => {
+            getBtnID(btn.category_id)
+            const btns = document.querySelectorAll('.categori_btn');
+            for(const btn of btns){
+                btn.classList.remove('bg-indigo-800');
+            }
+            button.classList.add('bg-indigo-800');
+        })
         btnContiner.appendChild(button);
     }
 
 }
 const getBtnID = async (id = '1000') => {
-    console.log(id);
     const res = await fetch(`https://openapi.programming-hero.com/api/videos/category/${id}`)
     const data = await res.json();
     getVideos(data.data);
 }
-
+const isSort = false;
 const getVideos = (videos) => {
+    if(isSort){
+        videos.sort((a,b) => {
+            const a = a.videos?.views;
+            const b = b.videos?.views;
+            const aInNumber = parseFloat(a.replace('K', ''))
+            const bInNumber = parseFloat(b.replace('K', ''))
+            return aInNumber - bInNumber;
+        })
+    }
     const videoConatiner = document.getElementById('videos_contaienr');
     videoConatiner.textContent = '';
     const errorMsg = document.getElementById('error_msg');
-    if(videos.length === 0){
+    if (videos.length === 0) {
         errorMsg.classList.remove('hidden');
     }
-    else(
+    else (
         errorMsg.classList.add('hidden')
     )
-    console.log(videos);
     videos.map((video) => {
         verified = '';
-        if(video.authors[0].verified){
+        if (video.authors[0].verified) {
             verified = `<img src="assets/images/Group 3.png" alt=""></img>`
         }
         const div = document.createElement('div');
@@ -47,13 +61,13 @@ const getVideos = (videos) => {
         </div>
         <div class="flex mt-5">
             <div class="w-1/5 ">
-                <img class="rounded-full w-full h-1/2" src="${video.authors[0].profile_picture}" alt="">
+                <img class="rounded-full w-full h-1/2" src="${video.authors[0]?.profile_picture}" alt="">
             </div>
             <div class="w-4/5 ml-4">
                 <div class="title">
                     <h3 class="text-xl font-semibold">${video.title}</h3>
-                    <p class="flex gap-3 mt-2">${video.authors[0].profile_name} ${verified}</p>
-                    <p class="text-sm">${video.others.views} Views</p>
+                    <p class="flex gap-3 mt-2">${video.authors[0]?.profile_name} ${verified}</p>
+                    <p class="text-sm">${video.others?.views} Views</p>
                 </div>
             </div>
         </div>
