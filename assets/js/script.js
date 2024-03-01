9d739fd2d88aa1dfa8d4da2498c4d62f07cb9275
shahinexy <shahinexy@gmail.com>
2024-03-01 12:15:36 +0600

    get the btn api data

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,14 +6,21 @@ const allBtn = async () => {
 }
 
 const getBtn = (btns) => {
+    console.log(btns);
     const btnContiner = document.getElementById('btn_container');
     for(const btn of btns){
         const button = document.createElement('button');
         button.className = 'btn rounded-none text-lg font-semibold text-white bg-indigo-400 px-5 py-2';
         button.innerText = btn.category;
+        button.addEventListener('click', () => getBtnID(btn.category_id))
         btnContiner.appendChild(button);
     }
 
 }
-
+const getBtnID = async (id) => {
+console.log(id);
+    const res = await fetch(`https://openapi.programming-hero.com/api/videos/category/${id}`)
+    const data = await res.json();
+    console.log(data);
+}
 allBtn()
